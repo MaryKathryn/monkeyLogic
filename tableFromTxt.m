7ed3168c33cba3e2fca052d04d139843297692b1
MaryKathryn <mk.mcintosh@mail.utoronto.ca>
2018-03-26 16:47:29 -0400

    Update tableFromTxt.m
    deleted the 'info' section

--- tableFromTxt.m
+++ tableFromTxt.m
@@ -1,6 +1,6 @@
 function outputTable = tableFromTxt(txtFileName)
 
 outputTable = readtable(txtFileName);
-outputTable.Properties.VariableNames = {'Condition'    'Info'     'Frequency'    'Block'     'Timing_File'     'TaskObject1'      'TaskObject2'     'TaskObject3'};
+outputTable.Properties.VariableNames = {'Condition'     'Frequency'    'Block'     'Timing_File'     'TaskObject1'      'TaskObject2'     'TaskObject3'};
 
 end 
